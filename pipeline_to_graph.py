7a9e095c148c51ed9a87ad2a6e2f3e4f7113d045
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2020-06-23 10:37:23 -0400

    Add Original node to allow multiple branches pipelines

--- pipeline_to_graph.py
+++ pipeline_to_graph.py
@@ -10,7 +10,7 @@ class Step:
     """
 
     # Id of the given Node
-    idNum = 0
+    idNum = -1
 
     def __init__(self, name, hyperparams):
         """     
@@ -30,7 +30,7 @@ class Step:
             self.children.append((arg_type, child))
 
     def __str__(self, move = 0):
-        string = "| " * move + " + Step : {} \n".format(self.name)
+        string = "| " * move + " + Step {} : {} \n".format(self.id, self.name)
         for _, child in self.children:
             string += child.__str__(move + 1)
         return string
@@ -59,6 +59,7 @@ class Pipeline:
             Arguments:
                 steps {dict} -- Dictionary of primitives
         """
+        original = Step('Original data', None)
         extracted_step = []
         # Iterate over steps
         for step in steps:
@@ -67,19 +68,24 @@ class Pipeline:
                 hyperparams = step["hyperparams"] if "hyperparams" in step else "No hyperparams"
                 new_step = Step(step["primitive"]["python_path"].split('.')[3], hyperparams)
                 extracted_step.append(new_step)
-                
+
                 # If have arguments in previous step, update the link between node
                 if "arguments" in step:
                     for arg_type in step["arguments"]:
                         data = step["arguments"][arg_type]["data"]
+
+                        # Deal with list of parents
                         if not isinstance(data, list): 
                             data = [data]
+
                         for parent in data:
                             parent = parent.split('.')
                             if parent[0] == "steps":
                                 extracted_step[int(parent[1])].addChild(new_step, arg_type)
+                            elif parent[0] == "inputs" and int(parent[1]) == 0:
+                                original.addChild(new_step, arg_type)
                         
-        return extracted_step[0]
+        return original
     
     def visualize(self):
         """
